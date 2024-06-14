3a6885c49209b17e722cae27bba53deb74a0de77
githubuser-desktop <github-187061147.trifocals021@passinbox.com>
2024-06-14 21:58:53 +0800

    Begin Test Tag with bit swifting

--- main.js
+++ main.js
@@ -15,46 +15,77 @@ class Group {
 }
 
 class IdGenerator {
-    constructor(lastId) {
-        this.id = lastId;
+    constructor() {
+        this.id = 0;
         this.generate = function () {
-            lastId += 1;
-            return lastId;
+            id += 1;
+            return id;
         };
     }
 }
 
-class TagGenerator{
-    constructor(lastId) {
-    this.tagsMap = new Map();
-    this.tagIdGenerator = new IdGenerator(lastId);
+
+class TagController {
+    constructor() {
+        this.lastBookIndex = 0;
+        this.lastSwifted = 0;
+        this.tagBooks = new Map();
+        this.tagsMap = new Map();
+        this.tagBooks.set(this.lastBookIndex, this.tagsMap);
     }
-    generate(name){
-        let nextId = this.tagIdGenerator.generate();
-        while(this.tagsMap.has(nextId)){
+    #TagIdGenerate(){
+        let newId = 1<<this.lastSwifted;
+        this.lastSwifted +=1;
+        return newId;
+    }
+    
+    Create(name) {
+        // let nextId = this.#TagIdGenerateTagIdGenerate();
+        while (this.tagsMap.has(nextId)) {
             alert(`tagsMap has ${nextId}!`);
-            nextId = this.tagIdGenerator.generate();
+            // nextId = this.#TagIdGenerateTagIdGenerate();
         }
-        let tempTag = new Tag(nextId,name);
-        this.tagsMap.set(nextId,tempTag);
+        let tempTag = new Tag(nextId, name);
+        this.tagsMap.set(nextId, tempTag);
         return tempTag;
     }
 }
 
-var tidg = new IdGenerator(0)
-var fish = new Tag(tidg.generate(), "fish")
-var pork = new Tag(tidg.generate(), "pork")
-var gidg = new IdGenerator(0)
-var fg = new Group(gidg.generate(), "fishGroup")
+class ItemController{
+    constructor(){
+        this.
+        this.itemIdGenerator = new IdGenerator();
+    }
+}
+
+class GroupController {
+    constructor() {
+        this.groupsMap = new Map();
+        this.groupIdGenerator = new IdGenerator();
+    }
+    generate(name) {
+        let nextId = this.groupIdGenerator.generate();
+        while (this.groupsMap.has(nextId)) {
+            alert(`groupsMap has ${nextId}!`);
+            nextId = this.groupIdGenerator.generate();
+        }
+        let tempgroup = new Group(nextId, name);
+        this.groupsMap.set(nextId, tempgroup);
+        return tempgroup;
+    }
+}
+let tagControler = new TagController(0);
+let fish = tagControler.Create("fish");
+let pork = tagControler.Create("pork");
+let groupControler = new GroupController(0);
+let fg = groupControler.generate("fishGroup");
 fg.tags.push(fish)
-var pg = new Group(gidg.generate(), "porkGroup")
+let pg = groupControler.generate("porkGroup");
 pg.tags.push(pork)
 
-var md = new Tag(tidg.generate(), "maindish")
-var mdg = new Group(gidg.generate(), "mainDishGroup")
+let md = new Tag(tidg.generate(), "maindish")
+let mdg = new Group(gidg.generate(), "mainDishGroup")
 mdg.tags.push(md)
 mdg.items.push(fg, pg)
 //jsontest1
 //var a = document.createElement("div");a.innerHTML = JSON.stringify(mdg);document.body.appendChild(a)
-
-
